16a1ec96f63645d0578d4b64290117eb2a55fc61
FlyzZx <nicolasfaraci@outlook.com>
2018-02-11 01:24:54 +0100

    Update MainClass

--- src/MainClass.java
+++ src/MainClass.java
@@ -28,7 +28,7 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		Loader.load(opencv_core.class);
-		System.out.println("Librairies charg�es !");
+		System.out.println("Librairies chargées !");
 		HashMap<Integer, String> mappingClass = new HashMap<>();
 		
 		LogoFinder fz = new LogoFinder("DataImage/");
